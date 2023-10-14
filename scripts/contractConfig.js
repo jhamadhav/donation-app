3c00bdea2d7becf658e86f6e2f355e2a4c3b18b6
jhamadhav <jhamadhav28@gmail.com>
2023-10-15 00:30:23 +0530

    new local deployment

--- scripts/contractConfig.js
+++ scripts/contractConfig.js
@@ -1,122 +1,122 @@
 // contract creation block address details
 
 // first block address
-const contractAddress = "0x1117549e99Be9F4Dd5d6a64e0eBf3B017eB08d32";
+const contractAddress = "0x1e0900C5DdD35Ac01F088b1F090D87A0AaC66F66";
 
 // first block abi
 const abi = [
-  {
-    "inputs": [
-      {
-        "internalType": "string",
-        "name": "",
-        "type": "string"
-      }
-    ],
-    "name": "donations",
-    "outputs": [
-      {
-        "internalType": "string",
-        "name": "id",
-        "type": "string"
-      },
-      {
-        "internalType": "address",
-        "name": "userAddress",
-        "type": "address"
-      },
-      {
-        "internalType": "string",
-        "name": "time",
-        "type": "string"
-      },
-      {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "string",
-        "name": "id",
-        "type": "string"
-      },
-      {
-        "internalType": "string",
-        "name": "time",
-        "type": "string"
-      },
-      {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
-    ],
-    "name": "create",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "string",
-        "name": "id",
-        "type": "string"
-      }
-    ],
-    "name": "getDonation",
-    "outputs": [
-      {
-        "components": [
-          {
-            "internalType": "string",
-            "name": "id",
-            "type": "string"
-          },
-          {
-            "internalType": "address",
-            "name": "userAddress",
-            "type": "address"
-          },
-          {
-            "internalType": "string",
-            "name": "time",
-            "type": "string"
-          },
-          {
-            "internalType": "uint256",
-            "name": "amount",
-            "type": "uint256"
-          }
-        ],
-        "internalType": "struct DonationContract.Donation",
-        "name": "",
-        "type": "tuple"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  },
-  {
-    "inputs": [],
-    "name": "getAllDonations",
-    "outputs": [
-      {
-        "internalType": "string[]",
-        "name": "",
-        "type": "string[]"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  }
-]
+   {
+      inputs: [
+         {
+            internalType: "string",
+            name: "",
+            type: "string",
+         },
+      ],
+      name: "donations",
+      outputs: [
+         {
+            internalType: "string",
+            name: "id",
+            type: "string",
+         },
+         {
+            internalType: "address",
+            name: "userAddress",
+            type: "address",
+         },
+         {
+            internalType: "string",
+            name: "time",
+            type: "string",
+         },
+         {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256",
+         },
+      ],
+      stateMutability: "view",
+      type: "function",
+      constant: true,
+   },
+   {
+      inputs: [
+         {
+            internalType: "string",
+            name: "id",
+            type: "string",
+         },
+         {
+            internalType: "string",
+            name: "time",
+            type: "string",
+         },
+         {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256",
+         },
+      ],
+      name: "create",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+   },
+   {
+      inputs: [
+         {
+            internalType: "string",
+            name: "id",
+            type: "string",
+         },
+      ],
+      name: "getDonation",
+      outputs: [
+         {
+            components: [
+               {
+                  internalType: "string",
+                  name: "id",
+                  type: "string",
+               },
+               {
+                  internalType: "address",
+                  name: "userAddress",
+                  type: "address",
+               },
+               {
+                  internalType: "string",
+                  name: "time",
+                  type: "string",
+               },
+               {
+                  internalType: "uint256",
+                  name: "amount",
+                  type: "uint256",
+               },
+            ],
+            internalType: "struct DonationContract.Donation",
+            name: "",
+            type: "tuple",
+         },
+      ],
+      stateMutability: "view",
+      type: "function",
+      constant: true,
+   },
+   {
+      inputs: [],
+      name: "getAllDonations",
+      outputs: [
+         {
+            internalType: "string[]",
+            name: "",
+            type: "string[]",
+         },
+      ],
+      stateMutability: "view",
+      type: "function",
+      constant: true,
+   },
+];
